e72d902fd9cacd01e75b8ba1c52d3875269d02b9
RyunVu <ryun3980@gmail.com>
2023-04-05 20:56:12 +0700

    Created Store Repository

--- src/WebWaterPaintStore.Services/WaterPaints/IStoreRepository.cs
+++ src/WebWaterPaintStore.Services/WaterPaints/IStoreRepository.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WebWaterPaintStore.Core.Contracts;
+using WebWaterPaintStore.Core.DTO;
+using WebWaterPaintStore.Core.Entities;
 
 namespace WebWaterPaintStore.Services.WaterPaints
 {
     public interface IStoreRepository 
     {
+        Task<Product> GetProductByIdAsync(
+            int id,
+            CancellationToken cancellationToken = default);
+
+        Task<Product> GetProductBySlugAsync(string slug,
+            CancellationToken cancellationToken = default);
+
+        Task<IPagedList<Product>> GetPagedProductsAsync(
+            IProductQuery productQuery,
+            IPagingParams pagingParams,
+            CancellationToken cancellationToken = default);
+
+        Task<IPagedList<T>> GetPagedProductsAsync<T>(
+            IProductQuery productQuery,
+            IPagingParams pagingParams,
+            Func<IQueryable<Product>, IQueryable<T>> mapper);
 
     }
 }
--- src/WebWaterPaintStore.Services/WaterPaints/StoreRepository.cs
+++ src/WebWaterPaintStore.Services/WaterPaints/StoreRepository.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using WebWaterPaintStore.Core.Contracts;
+using WebWaterPaintStore.Core.Entities;
+using WebWaterPaintStore.Data.Contexts;
+using WebWaterPaintStore.Services.Extensions;
 
 namespace WebWaterPaintStore.Services.WaterPaints
 {
     public class StoreRepository : IStoreRepository
     {
+        private readonly StoreDbContext _dbContext;
+
+        public StoreRepository(StoreDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        #region Product
+
+        public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<Product>()
+                .Include(c => c.Category)
+                .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+        }
+
+        public async Task<Product> GetProductBySlugAsync(string slug, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<Product>()
+               .Include(c => c.Category)
+               .FirstOrDefaultAsync(s => s.UrlSlug == slug, cancellationToken);
+        }
+
+        private IQueryable<Product> FilterProduct(IProductQuery productQuery)
+        {
+            var products = _dbContext.Set<Product>()
+                .Include(s => s.Category)
+                .WhereIf(productQuery.Year > 0, s => s.CreatedDate.Year == productQuery.Year)
+                .WhereIf(productQuery.Month > 0, s => s.CreatedDate.Month == productQuery.Month)
+                .WhereIf(productQuery.Day > 0, s => s.CreatedDate.Day == productQuery.Day)
+                .WhereIf(!string.IsNullOrEmpty(productQuery.CategorySlug), s => s.Category.UrlSlug.Contains(productQuery.CategorySlug))
+                .WhereIf(!string.IsNullOrEmpty(productQuery.ProductSlug), s => s.UrlSlug.Contains(productQuery.ProductSlug))
+                .WhereIf(!string.IsNullOrEmpty(productQuery.Keyword), s =>
+                    s.Name.Contains(productQuery.Keyword) ||
+                    s.ShortDescription.Contains(productQuery.Keyword) ||
+                    s.UrlSlug.Contains(productQuery.Keyword));
+            return products;
+        }
+
+        public Task<IPagedList<Product>> GetPagedProductsAsync(IProductQuery productQuery, IPagingParams pagingParams, CancellationToken cancellationToken = default)
+        {
+            return FilterProduct(productQuery).ToPagedListAsync(pagingParams, cancellationToken);
+        }
+
+        public async Task<IPagedList<T>> GetPagedProductsAsync<T>(IProductQuery productQuery, IPagingParams pagingParams, Func<IQueryable<Product>, IQueryable<T>> mapper)
+        {
+            var products = FilterProduct(productQuery);
+            var projectedProducts = mapper(products);
+
+            return await projectedProducts.ToPagedListAsync(pagingParams);
+        }
+
+
+        #endregion
+
     }
 }
