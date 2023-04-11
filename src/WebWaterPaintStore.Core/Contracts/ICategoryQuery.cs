620044e74bcc1062d8012364e7ec8a6093181297
RyunVu <ryun3980@gmail.com>
2023-04-11 21:29:31 +0700

    Created Category Query classes

---
+++ src/WebWaterPaintStore.Core/Contracts/ICategoryQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace WebWaterPaintStore.Core.Contracts
+{
+    public interface ICategoryQuery
+    {
+        public string Keyword { get; set; }
+        public bool Actived { get; set; }
+    }
+}
