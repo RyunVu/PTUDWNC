fce9780bb09938a034930546dd615a207a48c008
RyunVu <ryun3980@gmail.com>
2023-04-17 09:28:32 +0700

    Update UnitDetailDto.cs

--- src/WebWaterPaintStore.WebApi/Models/UnitDetailDto.cs
+++ src/WebWaterPaintStore.WebApi/Models/UnitDetailDto.cs
@@ -1,13 +1,16 @@
-﻿namespace WebWaterPaintStore.WebApi.Models
+﻿using WebWaterPaintStore.Core.Entities;
+
+namespace WebWaterPaintStore.WebApi.Models
 {
     public class UnitDetailDto
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
         public string UnitTag { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
         public float Discount { get; set; }
         public int SoldCount { get; set; }
+        public bool Actived { get; set; }
+        public int ProductId { get; set; }
     }
 }
