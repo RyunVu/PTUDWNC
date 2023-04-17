0da0f4c19a35493d9515831fa0a83e2c8cd6c85e
RyunVu <ryun3980@gmail.com>
2023-04-17 09:28:36 +0700

    Create UnitEditModel.cs

---
+++ src/WebWaterPaintStore.WebApi/Models/UnitEditModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebWaterPaintStore.WebApi.Models
+{
+    public class UnitEditModel
+    {
+        public string UnitTag { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public float Discount { get; set; }
+        public int SoldCount { get; set; }
+        public bool Actived { get; set; }
+        public int ProductId { get; set; }
+
+    }
+}
