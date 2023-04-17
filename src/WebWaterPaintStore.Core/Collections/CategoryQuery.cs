47f1a12898d1a5a17dfa456de31be2e0d4f4c710
RyunVu <ryun3980@gmail.com>
2023-04-17 09:26:48 +0700

    Configured Category Query

--- src/WebWaterPaintStore.Core/Collections/CategoryQuery.cs
+++ src/WebWaterPaintStore.Core/Collections/CategoryQuery.cs
@@ -5,6 +5,5 @@ namespace WebWaterPaintStore.Core.Collections
     public class CategoryQuery : ICategoryQuery
     {
         public string Keyword { get ; set ; }
-        public bool Actived { get ; set ; }
     }
 }
