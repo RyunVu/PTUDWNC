367688fd92168bd8d18f24df0bdf94a00011faaa
RyunVu <ryun3980@gmail.com>
2023-04-17 09:27:00 +0700

    Configured Unit Query

--- src/WebWaterPaintStore.Core/Contracts/IUnitQuery.cs
+++ src/WebWaterPaintStore.Core/Contracts/IUnitQuery.cs
@@ -3,13 +3,10 @@
     public interface IUnitQuery
     {
         public int? ProductId { get; set; }
-
         public string ProductSlug { get; set; }
         public string UnitTag { get; set; }
-
         public bool Actived { get; set; }
         public bool NotActived { get; set; }
-
         public string Keyword { get; set; }
     }
 }
