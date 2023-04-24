a102d0538f721cc03143738797452461bfc16d47
RyunVu <ryun3980@gmail.com>
2023-04-24 11:50:03 +0700

    Update ProductEditModel.cs

--- src/WebWaterPaintStore.WebApi/Models/ProductEditModel.cs
+++ src/WebWaterPaintStore.WebApi/Models/ProductEditModel.cs
@@ -8,9 +8,9 @@ namespace WebWaterPaintStore.WebApi.Models
         public string ShortDescription { get; set; }
         public string Meta { get; set; }
         public string UrlSlug { get; set; }
+        public bool Actived { get; set; }
         public string ImageUrl { get;set; }
-
         public int CategoryId { get; set; }
-        //public IList<UnitDetailDto> ProductUnitDetails { get; set; }
+        //public IList<UnitEditModel> UnitDetails { get; set; }
     }
 }
