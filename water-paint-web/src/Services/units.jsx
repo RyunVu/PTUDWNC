f4a226b3088a06eb007cb187a123848973d33524
RyunVu <ryun3980@gmail.com>
2023-04-25 13:40:42 +0700

    Update units.jsx

--- water-paint-web/src/Services/units.jsx
+++ water-paint-web/src/Services/units.jsx
@@ -2,45 +2,45 @@ import axios from 'axios';
 
 export async function getUnits() {
     const { data } = await axios.get(
-        `${process.env.REACT_APP_API_ENDPOINT}/Unit/unit?Actived=true&NotActived=false&PageSize=1000&PageNumber=1`,
+        `${process.env.REACT_APP_API_ENDPOINT}/product/unit?Actived=true&NotActived=false&PageSize=1000&PageNumber=1`,
     );
     if (data.isSuccess) return data.result;
     else return null;
 }
 
 export async function getUnitsByQueries(queries) {
-    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Unit?${queries}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/product/unit?${queries}`);
 
     if (data.isSuccess) return data.result;
     else return null;
 }
 
-export async function createUnit(Unit) {
-    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/Unit`, Unit);
+export async function createUnit(unit) {
+    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/product/unit`, unit);
 
     return data;
 }
 
-export async function updateUnit(id, Unit) {
-    const { data } = await axios.put(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`, Unit);
+export async function updateUnit(id, unit) {
+    const { data } = await axios.put(`${process.env.REACT_APP_API_ENDPOINT}/product/unit/${id}`, unit);
 
     return data;
 }
 
 export async function getUnitById(id = 0) {
-    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/product/unit/${id}`);
 
     if (data.isSuccess) return data.result;
     else return null;
 }
 
 export async function deleteUnitById(id) {
-    const { data } = await axios.delete(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`);
+    const { data } = await axios.delete(`${process.env.REACT_APP_API_ENDPOINT}/product/unit/${id}`);
     return data;
 }
 
 export async function toggleUnitActivedStatus(id) {
-    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/Unit/toggleUnit/${id}`);
+    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/product/unit/toggleUnit/${id}`);
 
     return data;
 }
