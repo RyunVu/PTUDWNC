1054ba27cfd3af0b78d8666e38e3de2e2c3132c9
RyunVu <ryun3980@gmail.com>
2023-04-25 07:56:40 +0700

    Update units.jsx

--- water-paint-web/src/Services/units.jsx
+++ water-paint-web/src/Services/units.jsx
@@ -2,8 +2,45 @@ import axios from 'axios';
 
 export async function getUnits() {
     const { data } = await axios.get(
-        `${process.env.REACT_APP_API_ENDPOINT}/product/unit?Actived=true&NotActived=false&PageSize=1000&PageNumber=1`,
+        `${process.env.REACT_APP_API_ENDPOINT}/Unit/unit?Actived=true&NotActived=false&PageSize=1000&PageNumber=1`,
     );
     if (data.isSuccess) return data.result;
     else return null;
+}
+
+export async function getUnitsByQueries(queries) {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Unit?${queries}`);
+
+    if (data.isSuccess) return data.result;
+    else return null;
+}
+
+export async function createUnit(Unit) {
+    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/Unit`, Unit);
+
+    return data;
+}
+
+export async function updateUnit(id, Unit) {
+    const { data } = await axios.put(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`, Unit);
+
+    return data;
+}
+
+export async function getUnitById(id = 0) {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`);
+
+    if (data.isSuccess) return data.result;
+    else return null;
+}
+
+export async function deleteUnitById(id) {
+    const { data } = await axios.delete(`${process.env.REACT_APP_API_ENDPOINT}/Unit/${id}`);
+    return data;
+}
+
+export async function toggleUnitActivedStatus(id) {
+    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/Unit/toggleUnit/${id}`);
+
+    return data;
 }
