987e63ebd0e9b29af1560f92a0794ead8a0da159
RyunVu <ryun3980@gmail.com>
2023-04-25 13:41:07 +0700

    Create UnitDetailInfo.cs

---
+++ src/WebWaterPaintStore.WebApi/Models/UnitDetailInfo.cs
@@ -0,0 +1,16 @@
+﻿using WebWaterPaintStore.Core.Entities;
+
+namespace WebWaterPaintStore.WebApi.Models
+{
+    public class UnitDetailInfo
+    {
+        public int Id { get; set; }
+        public string UnitTag { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public float Discount { get; set; }
+        public int SoldCount { get; set; }
+        public bool Actived { get; set; }
+        public ProductDto Product { get; set; }
+    }
+}
