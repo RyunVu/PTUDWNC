7a7ecec2096563dbcb4ad66ccd159f4be0d1f017
RyunVu <ryun3980@gmail.com>
2023-04-27 16:51:07 +0700

    Update index.jsx

--- water-paint-web/src/Pages/store/index.jsx
+++ water-paint-web/src/Pages/store/index.jsx
@@ -2,3 +2,4 @@ export { default as StoreHome } from './Home';
 export { default as StoreLayout } from './Layout';
 export { default as CategoryPage } from './Category';
 export { default as ContactPage } from './contact/Contact';
+export { default as ActionPage } from './userAction/userAction';
