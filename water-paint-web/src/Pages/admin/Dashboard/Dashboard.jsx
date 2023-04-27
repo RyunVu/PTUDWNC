35846c6a166bb0e856c6617a1ce737f12001f914
RyunVu <ryun3980@gmail.com>
2023-04-27 16:50:54 +0700

    Update Dashboard.jsx

--- water-paint-web/src/Pages/admin/Dashboard/Dashboard.jsx
+++ water-paint-web/src/Pages/admin/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from '../Layout/layout.module.scss';
 
 export default function Dashboard() {
-    return <div className="text">Dashboard</div>;
+    return <div className={styles.text}>Dashboard</div>;
 }
