4b39c139fd3eae31769dd8979c7f8382e7666d41
RyunVu <ryun3980@gmail.com>
2023-04-29 11:01:21 +0700

    Create index.jsx

---
+++ water-paint-web/src/routes/index.jsx
@@ -0,0 +1,37 @@
+import {
+    StoreHome,
+    ContactPage,
+    CategoryPage,
+    AdminHome,
+    Categories,
+    Products,
+    // Units,
+    Accounts,
+    ProductEdit,
+    UnitEdit,
+    AccountEdit,
+    CategoryEdit,
+} from '../Pages';
+
+const publicRoutes = [
+    { path: '/', component: StoreHome },
+    { path: '/store', component: StoreHome },
+    { path: 'store/category', component: CategoryPage },
+    { path: '/store/contact', component: ContactPage },
+];
+const privateRoute = [
+    { path: '/admin', component: AdminHome },
+    { path: '/admin/accounts', component: Accounts },
+    { path: '/admin/accounts/edit', component: AccountEdit },
+    { path: '/admin/accounts/edit/:id', component: AccountEdit },
+    { path: '/admin/categories', component: Categories },
+    { path: '/admin/categories/edit', component: CategoryEdit },
+    { path: '/admin/categories/edit/:id', component: CategoryEdit },
+    { path: '/admin/products', component: Products },
+    { path: '/admin/products/edit', component: ProductEdit },
+    { path: '/admin/products/edit/:id', component: ProductEdit },
+    { path: '/admin/products/:productId/add/units', component: UnitEdit },
+    { path: '/admin/products/edit/:id/units/:id', component: UnitEdit },
+];
+
+export { publicRoutes, privateRoute };
