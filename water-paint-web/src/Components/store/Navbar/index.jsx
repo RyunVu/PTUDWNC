089a5bb0f7931b45d7848d127a3697ecd6018a20
Moon1005 <tain3509@gmail.com>
2023-05-02 16:04:30 +0700

    Update header

--- water-paint-web/src/Components/store/Navbar/index.jsx
+++ water-paint-web/src/Components/store/Navbar/index.jsx
@@ -7,36 +7,38 @@ import styles from './Navbar.module.scss';
 
 const Navbar = () => {
     return (
-        <Container>
-            <Nb collapseOnSelect expand="sm" bg="white" variant="light" className="border-bottom shadow">
-                <div className="container-fluid">
-                    <Nb.Brand href="/">
-                        <img src={Logo} alt="logo" className={styles.logo} />
-                    </Nb.Brand>
-                    <Nb.Toggle aria-controls="responsive-navbar-nav" />
-                    <Nb.Collapse id="responsive-navbar-nav" className="d-sm-inline-flex justify-content-between">
-                        <Nav className="mr-auto flex-grow-1">
-                            <Nav.Item>
-                                <Link to="/" className={`nav-link text-dark ${styles.navLink}`}>
-                                    Trang chủ
-                                </Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Link to="/store/category" className={`nav-link text-dark ${styles.navLink}`}>
-                                    Sản phẩm
-                                </Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Link to="/store/contact" className={`nav-link text-dark ${styles.navLink}`}>
-                                    Liên hệ
-                                </Link>
-                            </Nav.Item>
-                        </Nav>
-                        <NavRight />
-                    </Nb.Collapse>
-                </div>
-            </Nb>
-        </Container>
+        <header className={styles.header}>
+            <Container>
+                <Nb collapseOnSelect expand="sm" bg="white" variant="light">
+                    <div className="container-fluid">
+                        <Nb.Brand href="/">
+                            <img src={Logo} alt="logo" className={styles.logo} />
+                        </Nb.Brand>
+                        <Nb.Toggle aria-controls="responsive-navbar-nav" />
+                        <Nb.Collapse id="responsive-navbar-nav" className="d-sm-inline-flex justify-content-between">
+                            <Nav className="mr-auto flex-grow-1">
+                                <Nav.Item>
+                                    <Link to="/" className={`nav-link text-dark ${styles.navLink}`}>
+                                        Trang chủ
+                                    </Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Link to="/store/category" className={`nav-link text-dark ${styles.navLink}`}>
+                                        Sản phẩm
+                                    </Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Link to="/store/contact" className={`nav-link text-dark ${styles.navLink}`}>
+                                        Liên hệ
+                                    </Link>
+                                </Nav.Item>
+                            </Nav>
+                            <NavRight />
+                        </Nb.Collapse>
+                    </div>
+                </Nb>
+            </Container>
+        </header>
     );
 };
 
