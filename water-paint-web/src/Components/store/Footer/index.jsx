ee2015d675fa739797109cb45b8321e3e946af0c
Moon1005 <tain3509@gmail.com>
2023-05-02 14:26:59 +0700

    Add grid

--- water-paint-web/src/Components/store/Footer/index.jsx
+++ water-paint-web/src/Components/store/Footer/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
     return (
         <footer className="border-top footer text-muted">
-            <div className="container-fluid text-center">&copy; Store</div>
+            <Container>
+                <Row>
+                    <Col>1 of 1</Col>
+                    <Col>1 of 1</Col>
+                    <Col>1 of 1</Col>
+                </Row>
+            </Container>
         </footer>
     );
 };
