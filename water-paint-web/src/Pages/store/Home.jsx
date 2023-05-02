4c74f0a7b27e1d2e2d405a681a368c56b80ed090
Moon1005 <tain3509@gmail.com>
2023-05-02 20:31:24 +0700

    Update home page UI

--- water-paint-web/src/Pages/store/Home.jsx
+++ water-paint-web/src/Pages/store/Home.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import Slider from '../../Components/store/Slider';
+import { getProductsByQueries } from '../../Services/products';
+import ProductInShop from '../../Components/store/ProductInShop';
 
 function Home() {
+    const [searchParams, setSearchParams] = useState({
+        PageSize: 10,
+        PageNumber: 1,
+        Actived: true,
+        SortOrder: 'DESC',
+    });
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const params = new URLSearchParams(searchParams);
+        getProductsByQueries(params).then((res) => {
+            if (res && res.items) setProducts(res.items);
+        });
+    }, [searchParams]);
+
     return (
         <div>
             <Slider />
+
+            <h4 className='mt-5' style={ {
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'red'
+            }}>SẢN PHẨM MỚI NHẤT</h4>
+            <Container className="mt-4 mb-4">
+                <Row>
+                    {products && products.length > 0 ? (
+                        products.map((product) => (
+                            <Col key={product.id} xs={4} className="mb-2">
+                                <ProductInShop product={product} />
+                            </Col>
+                        ))
+                    ) : (
+                        <p>Không có sản phẩm</p>
+                    )}
+                </Row>
+            </Container>
         </div>
     );
 }
